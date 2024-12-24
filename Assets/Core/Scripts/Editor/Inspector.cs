aedfcd0d34724c14d1c97c160ca4e3f9e666bffa
Paulo Kinjo <paulokinjo@hotmail.com>
2024-12-24 11:39:30 -0400

    (feat): New Stat UI Editor

---
+++ Assets/Core/Scripts/Editor/Inspector.cs
@@ -0,0 +1,13 @@
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Core.Scripts.Editor
+{
+    public class Inspector : InspectorElement
+    {
+        public new class UXmlFactory : UxmlFactory<Inspector, UxmlTraits>
+        {
+
+        }
+    }
+} 
---
+++ Assets/Core/Scripts/Editor/SplitView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UIElements;
+
+namespace Core.Scripts.Editor
+{
+    public class SplitView : TwoPaneSplitView
+    {
+        public new class UXmlFactory : UxmlFactory<SplitView, UxmlTraits>
+        {
+
+        }
+    }
+}
